5b1675d5143c3f136c37d9a4128eee292ec6c423
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-09-21 00:00:19 +0530

    Simple update

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,7 +4,7 @@ import TestimonialCard from "./TestimonialCard";
 
 const testimonialData = [
   {
-    companyName: "Webflow",
+    companyName: "Microsoft",
     desc: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."`,
     img: "/user-1.png",
     name: "Elliot John",
